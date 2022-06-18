b5f8ef6254ce973f8ab00484734a16c66a2f6141
OsvaldoC7 <osva.6cruz@gmail.com>
2022-06-17 21:13:41 -0500

    Init

---
+++ src/components/users/UserList.jsx
@@ -0,0 +1,44 @@
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import Button from '../common/Button'
+import React from 'react'
+import { deleteUser } from '../../features/users/usersSlice'
+
+export default function UserList() {
+  const users = useSelector(store => store.users)
+  const dispatch = useDispatch()
+
+  const handleDeleteUser = ({ id }) => {
+    dispatch(deleteUser({ id }))
+  }
+
+  const renderCard = () => users.map(user => (
+    <div key={user.id} className='bg-gray-300 p-4 flex flex-row items-center justify-between'>
+      <div className='flex flex-col'>
+        <span className='font-bold text-lg text-gray-700'>{ user.name }</span>
+        <span className='font-normal text-gray-600'>{ user.email }</span>
+      </div>
+      <div className='flex flex-row space-x-4'>
+        <Link to={`/edit-user/${user.id}`}>
+          <span className='fas fa-edit'></span>
+        </Link>
+        <Link to='#' onClick={() => handleDeleteUser({ id: user.id })}>
+          <span className='fas fa-trash'></span>
+        </Link>
+      </div>
+    </div>
+  ))
+
+  return (
+    <div>
+      <Link to='/add-user'>
+        <Button>Add user</Button>
+      </Link>
+      <div className='grid gap-4 md:grid-cols-2'>
+        {
+          users.length ? renderCard() : <p className='text-center col-span-2 text-gray-700 font-semibold'>No users</p>
+        }
+      </div>
+    </div>
+  )
+}
---
+++ src/features/users/usersSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = [
+  { id: '1', name: 'Panchito', email: 'panchito@email.com' },
+  { id: '2', name: 'Paquita', email: 'paquita@email.com' },
+]
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {
+    addUser: (state, action) => {
+      state.push(action.payload)
+    },
+
+    editUser: (state, action) => {
+      const { id, name, email } = action.payload
+      const user = state.find(user => user.id === id)
+      if(user) {
+        user.name = name
+        user.email = email
+      }
+    },
+
+    deleteUser: (state, action) => {
+      const { id } = action.payload
+      const user = state.find(user => user.id === id)
+      if(user) {
+        return state.filter(user => user.id !== id)
+      }
+    }
+  }
+})
+
+export const { addUser, editUser, deleteUser } = usersSlice.actions
+export default usersSlice.reducer
